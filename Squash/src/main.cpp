1f817ccd1fbb8fff6b6ecd042690d27ca13168cc
Karl <khansson.nr2@gmail.com>
2016-09-22 19:41:05 +0200

    Integrated a basic SFML window

--- Squash/src/main.cpp
+++ Squash/src/main.cpp
@@ -1,12 +1,38 @@
 #include <iostream>
 
+#include <SFML/Graphics.hpp>
+
 int main()
 {
 	using namespace std;
 
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Hello World!", sf::Style::Default);
+	
 	cout << "Hello World!" << endl;
 
-	cin.get();
+	sf::CircleShape shape(60, 4);
+
+	while (window.isOpen())
+	{
+		// check all the window's events that were triggered since the last iteration of the loop
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			// "close requested" event: we close the window
+			if (event.type == sf::Event::Closed)
+				window.close();
+			if (event.type == sf::Event::KeyPressed)
+				if (event.key.code == sf::Keyboard::Escape)
+					window.close();
+		}
+
+		window.clear();
+
+		window.draw(shape);
+
+		window.display();
+
+	}
 
 	return 0;
 }
