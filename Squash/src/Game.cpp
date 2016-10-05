83a8fe16a6427f018f710b55093c0e263cdbb241
Karl <khansson.nr2@gmail.com>
2016-10-05 19:34:43 +0200

    Added Game class and gave it basic functionality (Most content in main has been commented out for now)

---
+++ Squash/src/Game.cpp
@@ -0,0 +1,124 @@
+#include "Game.h"
+
+#include <SFML/System/Time.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Keyboard.hpp>
+#include <SFML/Window/Event.hpp>
+
+Game::Game()
+	: m_Window()
+	, m_Framerate(0.f)
+	, m_Shape(20.f)
+{
+	m_Window.create(sf::VideoMode(1280, 720), "TITLE", sf::Style::Titlebar | sf::Style::Close, sf::ContextSettings(0, 0, 8));
+
+	m_Shape.setFillColor(sf::Color::Red);
+	m_Shape.setPosition(0.f, 400.f - m_Shape.getRadius() / 2.f);
+}
+
+Game::~Game()
+{
+}
+
+void Game::init()
+{
+	/*
+		Not sure what specifically should be put here, but from experience it tends
+		to be useful...
+
+		We can remove it if we never have a need for it.
+	*/
+}
+
+void Game::run()
+{
+	/*
+		The game-loop. The heart of the beast.
+
+		Current setup:
+			- Static timestep
+				Good for physics. Slow downs do not affect results.
+				Bad for framerates. Sacrifice frames for more updates.
+	*/
+
+	sf::Clock timer;
+
+	float currentDelay = timer.restart().asSeconds();
+	float step = 1.f/60.f;
+
+	while (m_Window.isOpen())
+	{
+		currentDelay += timer.restart().asSeconds();
+		m_Framerate = 1.f / currentDelay;
+
+		while(currentDelay >= step)
+		{
+			handleEvents();
+			update(step);
+			currentDelay -= step;
+		}
+
+		draw();
+	}
+
+}
+
+void Game::handleEvents()
+{
+	sf::Event event;
+	while (m_Window.pollEvent(event))
+	{
+		switch (event.type)
+		{
+		case sf::Event::Closed:
+			m_Window.close(); 
+			break;
+		case sf::Event::KeyPressed:
+			if (event.key.code == sf::Keyboard::Escape)
+				m_Window.close(); 
+			break;
+		}
+
+	}
+}
+
+void Game::update(float dt)
+{
+	// Temporary : Just for testing vvvvv
+	static bool moveRight = true;
+	
+	if (moveRight)
+		m_Shape.move(150.f * dt, 0.f);
+	else
+		m_Shape.move(-150.f * dt, 0.f);
+	
+	if (m_Shape.getPosition().x <= 0.f && !moveRight)
+		moveRight = true;
+	else if (m_Shape.getPosition().x + 2.f * m_Shape.getRadius() >= 1280 && moveRight)
+		moveRight = false;
+	// Temporary : Just for testing ^^^^^
+
+
+	/*
+		Insert updates here
+	*/
+}
+
+void Game::draw()
+{
+	// Placeholder: FPS is displayed in the title-bar of the window
+	// Should be replaced with actual text on the screen.
+	m_Window.setTitle("fps: " + std::to_string(m_Framerate));
+
+	// No draw calls before this.
+	m_Window.clear();
+
+	m_Window.draw(m_Shape);
+
+	/*
+		Insert draw calls here	
+	*/
+
+	// No draw calls after this.
+	m_Window.display();
+}
---
+++ Squash/src/Game.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#ifndef GAME_H
+#define GAME_H
+
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
+
+class Game
+{
+public:
+	Game();
+	~Game();
+
+	void init();
+	
+	void run();
+
+	void handleEvents();
+	void update(float dt);
+	void draw();
+
+private:
+	sf::RenderWindow	m_Window;
+	float				m_Framerate;
+
+	sf::CircleShape		m_Shape;	// A temporary test shape
+
+};
+
+
+
+
+#endif
