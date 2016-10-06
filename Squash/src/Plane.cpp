8def8c2c600bbdcf1484806f6f91d9327d5902ad
Karl <khansson.nr2@gmail.com>
2016-10-06 11:00:03 +0200

    Began filling out the Plane class

--- Squash/src/Plane.cpp
+++ Squash/src/Plane.cpp
@@ -1,1 +1,18 @@
 ﻿#include "Plane.h"
+
+Plane::Plane(const sf::Vector3f&normal, float d)
+	: m_Normal(normal)
+	, m_D(d)
+{
+
+}
+
+Plane::Plane(const sf::Vector3f& normal, const sf::Vector3f& point)
+	: m_Normal(normal)
+{
+	
+}
+
+Plane::~Plane()
+{
+}
--- Squash/src/Plane.h
+++ Squash/src/Plane.h
@@ -3,11 +3,22 @@
 #ifndef PLANE_H
 #define PLANE_H
 
+#include <SFML/System/Vector3.hpp>
+
 class Plane
 {
 public:
+	Plane(const sf::Vector3f& normal, float d);
+	Plane(const sf::Vector3f& normal, const sf::Vector3f& point);
+	~Plane();
+
+
 
 private:
+	sf::Vector3f	m_Normal;
+	float			m_D;
+
+
 };
 
 #endif //PLANE
