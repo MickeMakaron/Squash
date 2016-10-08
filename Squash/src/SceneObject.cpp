6dfa4ccdf1d0a3794950d3b46b144c5fe94fea91
Mikael Hernvall <mikael.hernvall@gmail.com>
2016-10-08 10:23:03 +0200

    Moved common code for scene objects into SceneObject

---
+++ Squash/src/SceneObject.cpp
@@ -0,0 +1,39 @@
+﻿#include "SceneObject.h"
+
+#include <SFML/Graphics/RenderTarget.hpp>
+#include <SFML/Graphics/Texture.hpp>
+#include "CommonMath.h"
+
+SceneObject::SceneObject()
+{
+	setPosition({ 0,0,0 });
+}
+
+void SceneObject::setTexture(const std::shared_ptr<sf::Texture>& texture)
+{
+    m_Texture.reset(texture.get());
+	m_Sprite.setTexture(*texture);
+
+	float spriteWidth = m_Sprite.getGlobalBounds().width;
+	float spriteHeight = m_Sprite.getGlobalBounds().height;
+	m_Sprite.setOrigin(spriteWidth / 2.f, spriteHeight);
+
+}
+
+void SceneObject::setPosition(const sf::Vector3f& pos)
+{
+	m_Position = pos;
+	m_Sprite.setPosition(isometricProjection(m_Position));
+}
+
+void SceneObject::move(const sf::Vector3f delta)
+{
+	m_Position += delta;
+	m_Sprite.setPosition(isometricProjection(m_Position));
+}
+
+void SceneObject::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+	target.draw(m_Sprite, states);
+
+}
---
+++ Squash/src/SceneObject.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#ifndef SCENE_OBJECT_H
+#define SCENE_OBJECT_H
+
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/System/Vector3.hpp>
+
+#include <memory>
+
+class SceneObject : public sf::Drawable
+{
+public:
+	SceneObject();
+
+	virtual void setTexture(const std::shared_ptr<sf::Texture>& texture);
+
+	virtual void setPosition(const sf::Vector3f& pos);
+	virtual void move(const sf::Vector3f delta);
+
+protected:
+	void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+
+private:
+    std::shared_ptr<sf::Texture> m_Texture;
+	sf::Sprite      m_Sprite;
+	sf::Vector3f    m_Position;
+
+};
+
+#endif //SCENE_OBJECT_H
