7878aa7b50c300c7593800817e7e7e15c0042206
Mikael Hernvall <mikael.hernvall@gmail.com>
2016-10-08 16:03:27 +0200

    Added collision handler for ball-plane collisions

---
+++ Squash/src/physics.cpp
@@ -0,0 +1,20 @@
+﻿#include "physics.h"
+#include "CommonMath.h"
+
+#include "Ball.h"
+
+void handleCollision(Ball& ball, const std::vector<Plane>& planes)
+{
+    for(const Plane& plane : planes)
+    {
+        const sf::Vector3f CENTER = ball.getPosition();
+        const float RADIUS = ball.getRadius();
+        const sf::Vector3f PLANE_NORMAL = plane.getNormal();
+        float distance = dot(CENTER, PLANE_NORMAL) - plane.getD();
+        if(std::fabs(distance) < RADIUS)
+        {
+            ball.move(PLANE_NORMAL * (RADIUS - distance));
+            ball.accelerate(PLANE_NORMAL * length(ball.getVelocity()) * 1.5f);
+        }
+    }
+}
---
+++ Squash/src/physics.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#ifndef PHYSICS_H
+#define PHYSICS_H
+
+#include "Plane.h"
+#include <vector>
+class Ball;
+
+
+void handleCollision(Ball& ball, const std::vector<Plane>& planes);
+
+#endif //PHYSICS_H
