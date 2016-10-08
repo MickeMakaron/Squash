394c19c3667d1b8c274e4e82a5a913e91a76fcbd
Mikael Hernvall <mikael.hernvall@gmail.com>
2016-10-08 10:59:38 +0200

    Moved constants into separate header

---
+++ Squash/src/constants.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+namespace Constants
+{
+    const float TILE_SIZE = 60.f;
+    const unsigned int WINDOW_WIDTH = 1280;
+    const unsigned int WINDOW_HEIGHT = 720;
+    const char* const WINDOW_TITLE = "SQWOOOSH!";
+    const float FRAMES_PER_SECOND = 1.f / 60.f;
+}
+
+#endif //CONSTANTS_H
