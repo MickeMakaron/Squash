d0bc672c1969794233c3fccb7835d55ba2c391d2
Mikael Hernvall <mikael.hernvall@gmail.com>
2016-10-08 16:02:52 +0200

    Added Ball class

---
+++ Squash/src/Ball.cpp
@@ -0,0 +1,21 @@
+﻿#include "Ball.h"
+
+
+
+Ball::Ball(float radius)
+: m_Radius(radius)
+{
+}
+
+
+float Ball::getRadius() const
+{
+    return m_Radius;
+}
+
+
+void Ball::setTexture(const std::shared_ptr<sf::Texture>& texture)
+{
+    SceneObject::setTexture(texture);
+    setSize(m_Radius, m_Radius);
+}
---
+++ Squash/src/Ball.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#ifndef BALL_H
+#define BALL_H
+
+#include "SceneObject.h"
+
+class Ball : public SceneObject
+{
+    public:
+        explicit Ball(float radius);
+        float getRadius() const;
+
+        virtual void setTexture(const std::shared_ptr<sf::Texture>& texture) override;
+
+    private:
+        float m_Radius;
+};
+
+#endif //BALL_H
