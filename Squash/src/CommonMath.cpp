57fb7d813c186bbdb51ed29f0125594f456e82b6
Mikael Hernvall <mikael.hernvall@gmail.com>
2016-10-14 18:19:41 +0200

    rotateTransform now handles zero-length euler angle

--- Squash/src/CommonMath.cpp
+++ Squash/src/CommonMath.cpp
@@ -95,6 +95,9 @@ sf::Vector3f rotate(const sf::Vector3f& p, const sf::Vector3f& axis, float angle
 Quaternion rotateTransform(const Quaternion& q, const sf::Vector3f& eulerAngle)
 {
     float angle = length(eulerAngle);
+    if(angle < 0.0001f && angle > -0.0001f)
+        return q;
+
     sf::Vector3f axis = eulerAngle / angle;
     Quaternion rot = createRotationQuaternion(axis, angle);
     return rot * q;
